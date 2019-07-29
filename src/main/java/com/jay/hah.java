7a3ca66709bba98e6da8f8fab2e08381ea9f7f80
gugujie <15000938805@163.com>
2019-07-29 10:05:25 +0800

    第一次提交

---
+++ src/main/java/com/jay/hah.java
@@ -0,0 +1,17 @@
+package com.jay;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hah")
+public class hah extends HttpServlet {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+
+}
---
+++ src/main/java/dao_test.java
@@ -0,0 +1,5 @@
+public class dao_test {
+    public static void main(String[] args) {
+        System.out.print("haha");
+    }
+}
